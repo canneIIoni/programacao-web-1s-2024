2f2cdfe105e86707cac733a0496f2ecd43e612e4
Luca <paesberaldiluca@gmail.com>
2024-03-18 13:58:38 -0300

    Create variaveisOperadores.js
    Criação do arquivo .js, criação variáveis, realização das operações e impressão dos resultados.

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,20 @@
+var sum1 = 3;
+var sum2 = 4;
+var sub1 = 10
+var sub2 = 6
+var multi1 = 3
+var multi2 = 7
+var div1 = 12
+var div2 = 6
+
+// Realização das operações e armazenamento dos resultados
+let sum = sum1 + sum2;
+let subtraction = sub1 - sub2;
+let multiplication = multi1 * multi2;
+let division = div1 / div2;
+
+// Impressão dos resultados
+console.log(`\nResultado da soma entre ${sum1} e ${sum2}: ${sum}\n`);
+console.log(`Resultado da subtração entre ${sub1} e ${sub2}: ${subtraction}\n`);
+console.log(`Resultado da multiplicação entre ${multi1} e ${multi2}: ${multiplication}\n`);
+console.log(`Resultado da divisão entre ${div1} e ${div2}: ${division}`);
